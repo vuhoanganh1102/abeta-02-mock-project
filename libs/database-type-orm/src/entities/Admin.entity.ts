d65a2459b64c69532369c26a9b8f5618f8db0145
nhtqung <nhtqung17@gmail.com>
2024-05-20 16:39:16 +0700

    fix: fix field name on admin and user

--- libs/database-type-orm/src/entities/Admin.entity.ts
+++ libs/database-type-orm/src/entities/Admin.entity.ts
@@ -5,27 +5,24 @@ export class Admin {
     @PrimaryGeneratedColumn({ name: 'id', type: 'bigint', unsigned: true })
     id: number;
 
-    @Column({ type: 'varchar', length: 255, unique: true })
+    @Column({ name: 'email', type: 'varchar', length: 255, unique: true })
     email: string;
 
-    @Column({ type: 'varchar', length: 255 })
+    @Column({ name: 'password', type: 'varchar', length: 255 })
     password: string;
 
-    @Column({ type: 'varchar', length: 255, nullable: true })
+    @Column({ name: 'refresh_token', type: 'varchar', length: 255, nullable: true })
     refreshToken: string;
 
-    @Column({ type: 'varchar', length: 255, nullable: true })
+    @Column({ name: 'reset_token', type: 'varchar', length: 255, nullable: true })
     resetToken: string;
 
-    @Column({ type: 'tinyint', default: 0 })
-    isVerified: number;
+    @DeleteDateColumn({ name: 'deleted_at', type: 'datetime' })
+    deletedAt: string;
 
-    @CreateDateColumn()
-    createdAt: Date;
+    @CreateDateColumn({ name: 'created_at', type: 'datetime' })
+    createdAt: string;
 
-    @DeleteDateColumn()
-    deletedAt: Date;
-
-    @UpdateDateColumn()
-    updatedAt: Date;
+    @UpdateDateColumn({ name: 'updated_at', type: 'datetime' })
+    updatedAt: string;
 }
--- libs/database-type-orm/src/entities/User.entity.ts
+++ libs/database-type-orm/src/entities/User.entity.ts
@@ -5,42 +5,42 @@ export class User {
     @PrimaryGeneratedColumn({ name: 'id', type: 'bigint', unsigned: true })
     id: number;
 
-    @Column({ type: 'varchar', length: 255, unique: true })
+    @Column({ name: 'email', type: 'varchar', length: 255, unique: true })
     email: string;
 
-    @Column({ type: 'varchar', length: 255, nullable: true })
+    @Column({ name: 'first_name', type: 'varchar', length: 255, nullable: true })
     firstName: string;
 
-    @Column({ type: 'varchar', length: 255, nullable: true })
+    @Column({ name: 'last_name', type: 'varchar', length: 255, nullable: true })
     lastName: string;
 
-    @Column({ type: 'varchar', length: 255 })
+    @Column({ name: 'password',type: 'varchar', length: 255 })
     password: string;
 
-    @Column({ type: 'varchar', length: 20, unique: true, nullable: true })
+    @Column({ name: 'phone_number', type: 'varchar', length: 20, unique: true, nullable: true })
     phoneNumber: string;
 
-    @Column({ type: 'varchar', length: 255, nullable: true })
+    @Column({ name: 'date_of_birth', type: 'varchar', length: 255, nullable: true })
     dateOfBirth: string;
 
-    @Column({ type: 'varchar', length: 255 })
+    @Column({ name: 'avatar',type: 'varchar', length: 255 })
     avatar: string;
 
-    @Column({ type: 'varchar', length: 255, nullable: true })
+    @Column({ name: 'refresh_token', type: 'varchar', length: 255, nullable: true })
     refreshToken: string;
 
-    @Column({ type: 'varchar', length: 255, nullable: true })
+    @Column({ name: 'reset_token', type: 'varchar', length: 255, nullable: true })
     resetToken: string;
 
-    @Column({ type: 'tinyint', default: 0 })
+    @Column({ name: 'is_verify',type: 'tinyint', default: 0 })
     isVerified: number;
 
-    @CreateDateColumn()
-    createdAt: Date;
+    @DeleteDateColumn({ name: 'deleted_at', type: 'datetime' })
+    deletedAt: string;
 
-    @DeleteDateColumn()
-    deletedAt: Date;
+    @CreateDateColumn({ name: 'created_at', type: 'datetime' })
+    createdAt: string;
 
-    @UpdateDateColumn()
-    updatedAt: Date;
+    @UpdateDateColumn({ name: 'updated_at', type: 'datetime' })
+    updatedAt: string;
 }
